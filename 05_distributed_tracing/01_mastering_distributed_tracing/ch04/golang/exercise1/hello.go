9a4742f2d5b0b751b3fc92067b2f04863ea1cfbe
gurumee <gurumee92@gmail.com>
2021-02-03 22:42:07 +0900

    exercise2 끝

--- 05_distributed_tracing/01_mastering_distributed_tracing/ch04/golang/exercise1/hello.go
+++ 05_distributed_tracing/01_mastering_distributed_tracing/ch04/golang/exercise1/hello.go
@@ -5,13 +5,22 @@ import (
 	"net/http"
 	"strings"
 	"studydts/exercise1/people"
+	"studydts/lib/tracing"
+
+	"github.com/opentracing/opentracing-go"
+	otLog "github.com/opentracing/opentracing-go/log"
 )
 
 var repo *people.Repository
+var tracer opentracing.Tracer
 
 func main() {
 	repo = people.NewRepository()
 	defer repo.Close()
+
+	tr, closer := tracing.Init("go-2-hello")
+	defer closer.Close()
+	tracer = tr
 
 	http.HandleFunc("/sayHello/", handleSayHello)
 	log.Println("Listen on http://localhost:8080/")
@@ -19,23 +28,35 @@ func main() {
 }
 
 func handleSayHello(w http.ResponseWriter, r *http.Request) {
+	span := tracer.StartSpan("say-hello")
+	defer span.Finish()
+
 	name := strings.TrimPrefix(r.URL.Path, "/sayHello/")
-	greeting, err := SayHello(name)
+	greeting, err := SayHello(name, span)
 
 	if err != nil {
+		span.SetTag("error", true)
+		span.LogFields(otLog.Error(err))
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	span.SetTag("response", greeting)
 	w.Write([]byte(greeting))
 }
 
-func SayHello(name string) (string, error) {
+func SayHello(name string, span opentracing.Span) (string, error) {
 	person, err := repo.GetPerson(name)
 
 	if err != nil {
 		return "", err
 	}
+
+	span.LogKV(
+		"name", person.Name,
+		"title", person.Title,
+		"description", person.Description,
+	)
 
 	return FormatGreeting(
 		person.Name,
--- 05_distributed_tracing/01_mastering_distributed_tracing/ch04/golang/go.mod
+++ 05_distributed_tracing/01_mastering_distributed_tracing/ch04/golang/go.mod
@@ -2,4 +2,11 @@ module studydts
 
 go 1.14
 
-require github.com/go-sql-driver/mysql v1.5.0
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/uber/jaeger-client-go v2.25.0+incompatible
+	github.com/uber/jaeger-lib v2.4.0+incompatible // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
