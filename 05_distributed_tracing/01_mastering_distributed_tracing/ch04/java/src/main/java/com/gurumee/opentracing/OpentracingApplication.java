eb47cc3562aac79f08eefbda604a9502b4575dc4
gurumee <gurumee92@gmail.com>
2021-02-12 15:26:21 +0900

    UPDATE ch04 ex0. java project setting

---
+++ 05_distributed_tracing/01_mastering_distributed_tracing/ch04/java/src/main/java/com/gurumee/opentracing/OpentracingApplication.java
@@ -0,0 +1,13 @@
+package com.gurumee.opentracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentracingApplication.class, args);
+	}
+
+}
---
+++ 05_distributed_tracing/01_mastering_distributed_tracing/ch04/java/src/test/java/com/gurumee/opentracing/OpentracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurumee.opentracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpentracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
