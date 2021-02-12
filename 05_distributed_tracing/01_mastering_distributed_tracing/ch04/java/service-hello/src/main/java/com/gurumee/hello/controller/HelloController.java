656061b45148cbe0390f610b03433d2ab2058d92
gurumee <gurumee92@gmail.com>
2021-02-12 21:48:05 +0900

    ch04.ex02.02 create span

--- 05_distributed_tracing/01_mastering_distributed_tracing/ch04/java/service-hello/src/main/java/com/gurumee/hello/controller/HelloController.java
+++ 05_distributed_tracing/01_mastering_distributed_tracing/ch04/java/service-hello/src/main/java/com/gurumee/hello/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.gurumee.hello.controller;
 
 import com.gurumee.hello.lib.model.Person;
 import com.gurumee.hello.lib.model.PersonRepository;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +15,18 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class HelloController {
     private final PersonRepository personRepository;
+    private final Tracer tracer;
 
     @GetMapping("/sayHello/{name}")
     public String sayHello(@PathVariable String name) {
-        Person person = getPerson(name);
-        String response = formatGreeting(person);
-        return response;
+        Span span = tracer.buildSpan("say-hello").start();
+        try {
+            Person person = getPerson(name);
+            String response = formatGreeting(person);
+            return response;
+        } finally {
+            span.finish();
+        }
     }
 
     private String formatGreeting(Person person) {
